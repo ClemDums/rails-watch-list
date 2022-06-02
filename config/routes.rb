00512fe9d94e36b186532cc26660f8f8d7a01f3f
Clemence Dumont <cdumont90@gmail.com>
2022-06-02 19:26:07 +0200

    watch list partie 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # post 'lists', to: 'lists#create'
+  # get 'lists/list:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'bookmarks', to: 'bookmarks#create'
+  # get 'bookmarks/:id', to: 'bookmarks#show', as: :bookmark
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
